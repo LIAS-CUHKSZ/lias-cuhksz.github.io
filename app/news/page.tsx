850f23d0c395622d75a567fb11d84f76957699fb
3170105790 <840482616@qq.com>
2023-12-07 11:03:15 +0800

    test

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -10,7 +10,7 @@ function Page(): JSX.Element {
       <div className="mx-auto mb-20 max-w-5xl items-start justify-center p-4 md:mt-20">
         <h2 className="mb-4 text-2xl font-semibold">News</h2>
         <div>
-          <p> A paper was accepted by TII (2023.10.22)</p>
+          <p>● A paper was accepted by TII (2023.10.22)</p>
           <p>
             Shuaiting Huang, Chengcheng Zhao, Lingying Huang, Peng Cheng,
             Junfeng Wu and Lin Cai. Plug-and-Play Distributed Estimation of
@@ -25,9 +25,7 @@ function Page(): JSX.Element {
               IEEE Transactions on Industrial Informatics
             </a>
           </p>
-          <br />
-          <br />
-          <p>Prof. Shiyu Zhao visited LIAS! (2023.07.17)</p>
+          <p>● Prof. Shiyu Zhao visited LIAS! (2023.07.17)</p>
           <p>
             <a
               href="https://www.westlake.edu.cn/faculty/shiyu-zhao.html"
@@ -37,9 +35,7 @@ function Page(): JSX.Element {
               Shiyu Zhao
             </a>
           </p>
-          <br />
-          <br />
-          <p>Prof. Dawei Shi visited LIAS! (2023.07.15)</p>
+          <p>● Prof. Dawei Shi visited LIAS! (2023.07.15)</p>
           <p>
             <a
               href="https://ac.bit.edu.cn/szdw/dsmd/bssds/58156bb085df4b82b7dae77bc20873cb.htm"
@@ -49,9 +45,7 @@ function Page(): JSX.Element {
               Dawei Shi
             </a>
           </p>
-          <br />
-          <br />
-          <p>A paper was accepted by CDC (2023.07.12)</p>
+          <p>● A paper was accepted by CDC (2023.07.12)</p>
           <p>
             Jianhui Li, Youcheng Niu, Shuang Li, Yuzhe Li, Jinming Xu and
             Junfeng Wu. Inducing Desired Equilibrium in Taxi Repositioning
@@ -66,11 +60,9 @@ function Page(): JSX.Element {
               https://cdc2023.ieeecss.org/
             </a>
           </p>
-          <br />
-          <br />
           <p>
-            Shiyu Chen has received the master&apos;s degree offer from Cornell
-            University, congratulations! (2023.04.01)
+            ● Shiyu Chen has received the master&apos;s degree offer from
+            Cornell University, congratulations! (2023.04.01)
           </p>
           <p>
             <a
