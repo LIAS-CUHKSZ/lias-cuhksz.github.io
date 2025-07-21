3d10e20e211962ae3522d828f85cdfed2803c581
Effun <976127939@qq.com>
2025-07-21 12:54:22 +0800

    Update page.tsx

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
 const newsItems: React.ReactNode[] = [
   <>
     A paper was conditionally accepted by 2025 IEEE TCNS(2025.07.05).
-    <br /> 
+    <br />
     Shuaiting Huang, Haodong Jiang, Chengcheng Zhao, Peng Cheng, Junfeng Wu, 
     Fully Distributed State Estimation for Multi-agent Systems and its Application 
     in Cooperative Localization, IEEE Transactions on Control of Network Systems, 
