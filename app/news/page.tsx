20979cb2e33aa7c497f63128458a595ee9ac04da
Effun <976127939@qq.com>
2025-07-21 11:45:46 +0800

    update news1

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -9,11 +9,10 @@ export const metadata: Metadata = {
 // Using JSX elements directly is easiest if you have links/formatting inside
 const newsItems: React.ReactNode[] = [
   <>
-    A paper was accepted by 2025 IEEE RAL(2025.03.31).
-    <br /> Wenliang Sheng, Hongxu Zhao, Lingpeng Chen, Guangyang Zeng, Yunling
-    Shao, Yuze Hong, Chao Yang, Ziyang Hong, Junfeng Wu, BESTAnP: Bi-Step
-    Efficient and Statistically Optimal Estimator for Acoustic-n-Point Problem,
-    IEEE Robotics and Automation Letters, accepted.
+    A paper was conditionally accepted by 2025 IEEE TCNS(2025.07.05).
+    <br /> Shuaiting Huang, Haodong Jiang, Chengcheng Zhao, Peng Cheng, Junfeng
+    Wu, Fully Distributed State Estimation for Multi-agent Systems and its Application
+    in Cooperative Localization, IEEE Transactions on Control of Network Systems, conditionally accepted.
   </>,
   <>
     A paper was accepted by 2025 IEEE RAL(2025.03.31).
