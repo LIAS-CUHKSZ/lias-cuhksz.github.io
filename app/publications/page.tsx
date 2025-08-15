af9e51d355c909abe289abb82448dcbbe49d7861
Effun <976127939@qq.com>
2025-08-15 19:52:41 +0800

    Update page.tsx

--- app/publications/page.tsx
+++ app/publications/page.tsx
@@ -12,9 +12,9 @@ function Page(): JSX.Element {
         <div>
           <p>
             [54] Guangyang Zeng, Qingcheng Zeng, Xinghan Li, Biqiang Mu, Jiming
-            Chen, Ling Shi, Junfeng Wu, Consistent and Optimal Solution to Camera
-            Motion Estimation, IEEE Transactions on Pattern Analysis and Machine
-            Intelligence, accepted.
+            Chen, Ling Shi, Junfeng Wu, Consistent and Optimal Solution to
+            Camera Motion Estimation, IEEE Transactions on Pattern Analysis and
+            Machine Intelligence, accepted.
           </p>
           <p>
             [53] Jianhui Li, Bokang Zhang, Youcheng Niu, Shuang Wu, Kemi Ding,
